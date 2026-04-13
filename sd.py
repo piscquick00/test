410030e5eb0f4b3ca49e92383f190fb25194a826
piscquick00 <piscquick@protonmail.ch>
2026-04-13 20:50:28 +0200

    sd.py

--- sd.py
+++ sd.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 import socket
 
-# Impostazioni di configurazione
 HOST = '0.0.0.0'  # Ascolta su tutte le interfacce di rete
 PORT = 4444        # La porta su cui ascoltare
 
-# Crea il socket TCP/IP
+# Crea il socket TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # Associa il socket alla porta specificata
-    s.bind((HOST, PORT))
-    # Inizia ad ascoltare le connessioni
-    s.listen(1)
+    s.bind((HOST, PORT))  # Associa la porta
+    s.listen(1)  # Inizia ad ascoltare
     print(f"Mi metto in ascolto sulla porta {PORT}...")
 
     while True:
-        # Accetta una connessione
-        conn, addr = s.accept()
+        conn, addr = s.accept()  # Accetta connessioni
         with conn:
             print(f"Connessione ricevuta da {addr}")
             while True:
-                # Ricevi i dati
-                data = conn.recv(1024)
+                data = conn.recv(1024)  # Ricevi i dati
                 if not data:
-                    # Se non ci sono più dati, termina la connessione
                     break
                 print(f"Dati ricevuti: {data.decode('utf-8')}")
-                # Invia di nuovo i dati al client (echo)
-                conn.sendall(data)
+                conn.sendall(data)  # Ritorna i dati ricevuti
